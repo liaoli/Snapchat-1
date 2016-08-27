de07b4df3ce02a8ae8abe7323f27fc3d54fc1286
sinaa2000 <sahmadi@student.unimelb.edu.au>
2016-08-27 15:30:50 +1000

    "first commit"

---
+++ app/src/main/java/com/example/nocomment/snapchat/ChatList.java
@@ -0,0 +1,23 @@
+package com.example.nocomment.snapchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class ChatList extends AppCompatActivity {
+    ListView listView;
+    ArrayAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_list);
+
+        final String [] friends={"Rose","Jack"};
+        listView=(ListView)findViewById(R.id.listView);
+        adapter=new ArrayAdapter(ChatList.this, R.layout.listviewitem,friends);
+        listView.setAdapter(adapter);
+    }
+}
